a7fb9e0aee537f14db34e2f623e84cdfa2799958
bradhenry <brad.henry.49@gmail.com>
2019-06-08 00:26:47 -0400

    fix an issue where the bot would disconnect from the voice channel when a user would join the channel from a different channel that only had a single user left in it

--- src/main/java/net/dirtydeeds/discordsoundboard/listeners/DisconnectListener.java
+++ src/main/java/net/dirtydeeds/discordsoundboard/listeners/DisconnectListener.java
@@ -1,11 +1,14 @@
 package net.dirtydeeds.discordsoundboard.listeners;
 
 import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.util.List;
 
 public class DisconnectListener extends ListenerAdapter {
 
@@ -17,8 +20,13 @@ public class DisconnectListener extends ListenerAdapter {
     @Override
     public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
         VoiceChannel channel = event.getChannelLeft();
-        if (channel != null && channel.getMembers().size() == 1) {
-            Guild guild = channel.getGuild();
+        if (channel == null) {
+            return;
+        }
+
+        List<Member> members = channel.getMembers();
+        Guild guild = channel.getGuild();
+        if (members.size() == 1 && members.get(0).equals(guild.getSelfMember())) {
             LOG.info("Bot is the last user left in channel, disconnecting");
             guild.getAudioManager().closeAudioConnection();
         }
